a8f9123c6d3b89d1ba36cc5eb90ee3665ab04cd6
Carolina Andrade <cpandrade91@gmail.com>
2021-07-12 21:52:56 -0300

    Primeiras alterações

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,40 @@
 import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSidebar(propriedades) {
+  console.log(propriedades)
+  return (
+    <Box>
+      <img src={`https://github.com/${propriedades.githubUser}.png`} style={{borderRadius: '8px'}} />
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+
+  const githubUser = 'candrade-ti';
+
+  return (
+  <MainGrid>
+    <div className="profileArea" style={{ gridArea: 'profileArea'}}>
+      <ProfileSidebar githubUser={githubUser}/>
+    </div>
+    <div className="welcomeArea" style={{ gridArea: 'welcomeArea'}}>
+    <Box>
+      Bem vindo
+    </Box>
+    </div>
+    <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea'}}>
+    <Box>
+      Pessoas
+    </Box>
+    <Box>
+      Comunidades
+    </Box>
+    </div>  
+
+  </MainGrid>
+
+  )
 }
