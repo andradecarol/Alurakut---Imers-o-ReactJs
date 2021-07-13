f54877695064bafa10fb77519c961257fd459fd0
Carolina Andrade <cpandrade91@gmail.com>
2021-07-13 11:56:23 -0300

    Aula 1

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 
 function ProfileSidebar(propriedades) {
   console.log(propriedades)
@@ -14,27 +16,47 @@ function ProfileSidebar(propriedades) {
 export default function Home() {
 
   const githubUser = 'candrade-ti';
+  const pessoasFavoritas = ['billyjoe-ram', 'josee-fernandes', 'AndreSeoane', 'devMarcoAntonio', 'enzocsantos18', 'Eduardin-Dev', 
+  // 'PedroReinaldoBastos', 'HenriqueMartimianoGomesSalgado', 'Tamiris-Siqueira'
+  ];
 
   return (
-  <MainGrid>
-    <div className="profileArea" style={{ gridArea: 'profileArea'}}>
-      <ProfileSidebar githubUser={githubUser}/>
-    </div>
-    <div className="welcomeArea" style={{ gridArea: 'welcomeArea'}}>
-    <Box>
-      Bem vindo
-    </Box>
-    </div>
-    <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea'}}>
-    <Box>
-      Pessoas
-    </Box>
-    <Box>
-      Comunidades
-    </Box>
-    </div>  
-
-  </MainGrid>
-
+    <>
+    <AlurakutMenu/>
+    <MainGrid>   
+      <div className="profileArea" style={{ gridArea: 'profileArea'}}>
+        <ProfileSidebar githubUser={githubUser}/>
+      </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea'}}>
+        <Box>
+          <h1 className="title">
+          Bem vindo(a)
+          </h1>
+         <OrkutNostalgicIconSet/>
+        </Box>
+      </div>
+      <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea'}}>
+        <ProfileRelationsBoxWrapper>
+          <h2 className="smallTitle">Pessoas ({pessoasFavoritas.length})</h2>
+          
+          <ul>
+          {pessoasFavoritas.map((itemAtual) => {
+            return (
+              <li>
+                 <a href={`/users/${itemAtual}`} key={itemAtual}>
+                <img src={`https://github.com/${itemAtual}.png`} alt="foto de perfil do amigo" />
+                <span>{itemAtual}</span>
+              </a>
+              </li>             
+            )
+          })}
+          </ul>
+        </ProfileRelationsBoxWrapper>
+        {/* <Box>
+          Comunidades
+        </Box> */}
+      </div>  
+    </MainGrid>    
+    </>
   )
 }
